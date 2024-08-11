42e1a673d1b2a91fbd70a421a91aaf88fca7e264
Samuel <mmachado0222@gmail.com>
2024-08-11 11:49:53 -0300

    chore: add path alias for project structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@app': path.resolve(__dirname, 'src', 'app'),
+			'@ui': path.resolve(__dirname, 'src', 'ui'),
+		},
+	},
+	server: {
+		port: 3000,
+	},
+});
