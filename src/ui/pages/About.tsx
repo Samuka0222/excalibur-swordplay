996feaf7fe5c2620f49c444f3822d71fda595879
Samuel <mmachado0222@gmail.com>
2024-09-29 11:25:03 -0300

    refactor: create Plans Component and adjust the order of the plans

--- src/ui/pages/About.tsx
+++ src/ui/pages/About.tsx
@@ -1,7 +1,8 @@
 import { AnimatedContainer } from "@ui/components/AnimatedContainer";
-import { Check, Instagram, Swords } from "lucide-react";
+import { Instagram, Swords } from "lucide-react";
 import { motion } from "framer-motion";
 import { useEffect } from "react";
+import { Plans } from "@ui/components/Plans";
 
 export default function About() {
   useEffect(() => {
@@ -18,7 +19,7 @@ export default function About() {
         initial={{ y: 700 }}
         animate={{ y: 0 }}
         transition={{ duration: 1, ease: 'easeInOut' }}
-        className="w-full md:w-[85%] lg:w-[75%] xl:w-[70%] mt-10 xl:flex justify-center gap-24
+        className="w-full md:w-[85%] lg:w-[75%] mt-10 xl:flex justify-center gap-24
         "
       >
         <div className="w-full xl:w-[50%] flex flex-col gap-5">
@@ -41,7 +42,7 @@ export default function About() {
         initial={{ y: 700 }}
         animate={{ y: 0 }}
         transition={{ duration: 1, ease: 'easeInOut' }}
-        className="w-full md:w-[85%] lg:w-[75%] xl:w-[70%] mt-10 xl:flex justify-center gap-24"
+        className="w-full md:w-[85%] lg:w-[75%] mt-10 xl:flex justify-center gap-24"
       >
         <div className="w-full xl:w-[50%] flex flex-col gap-5">
           <h2 className="w-full text-2xl font-medieval font-bold flex">
@@ -58,7 +59,7 @@ export default function About() {
         initial={{ y: 700 }}
         animate={{ y: 0 }}
         transition={{ duration: 1, ease: 'easeInOut' }}
-        className="w-full md:w-[85%] lg:w-[75%] xl:w-[70%] mt-10 xl:flex justify-center gap-24"
+        className="w-full md:w-[85%] lg:w-[75%] mt-10 xl:flex justify-center gap-24"
       >
         <div className="w-full xl:w-[50%] flex flex-col gap-5">
           <h2 className="w-full text-2xl font-medieval font-bold flex">
@@ -68,66 +69,7 @@ export default function About() {
           <p className="text-lg indent-6 text-justify">
             <span className="text-3xl font-medieval font-medium">S</span>im, como foi dito anteriomente, o <span className="font-medium underline">Excalibur Swordplay</span> é independente, então iremos em breve disponibilizar campanhas para apoio financeiro.
           </p>
-          <div className="w-full flex flex-col md:flex-row justify-center items-center gap-4 md:gap-0 xl:gap-3 text-white mt-8">
-
-            <div className="bg-primary w-full h-[390px] flex flex-col gap-3 justify-center items-center border border-gray-200 rounded-lg">
-              <div className="w-full h-16 border-b border-gray-500 flex justify-center items-center">
-                <h3 className="font-bold text-xl"><span className="text-3xl font-medieval font-medium">N</span>obre</h3>
-              </div>
-              <ul className="w-full h-full flex flex-col gap-2 px-2">
-                <li className="flex gap-2"><Check /> Logo no Banner do evento</li>
-                <li className="flex gap-2"><Check /> Logo no site</li>
-                <li className="flex gap-2"><Check /> Bandeiras na arena com a logo</li>
-                <li className="flex gap-2"><Check /> Logo em material</li>
-                <li className="flex gap-2"><Check /> Divulgação da marca em vídeo</li>
-                <li className="flex gap-2"><Check /> Divulgação no Evento</li>
-              </ul>
-              <div className="w-full h-20 flex justify-center items-center border-t">
-                <p className="font-bold text-3xl ">
-                  R$ 100,00
-                </p>
-              </div>
-            </div>
-
-            <div className="bg-primary w-full h-[390px] flex flex-col gap-3 justify-center items-center border border-gray-200 rounded-lg">
-              <div className="w-full h-16 border-b border-gray-500 flex justify-center items-center">
-                <h3 className="font-bold text-xl"><span className="text-3xl font-medieval font-medium">C</span>avaleiro</h3>
-              </div>
-              <ul className="w-full h-full flex flex-col gap-2 px-2">
-                <li className="flex gap-2"><Check /> Logo no Banner do evento</li>
-                <li className="flex gap-2"><Check /> Logo no site</li>
-                <li className="flex gap-2 text-gray-600">Bandeiras na arena com a logo</li>
-                <li className="flex gap-2 text-gray-600"> Logo em material</li>
-                <li className="flex gap-2"><Check /> Divulgação da marca em vídeo</li>
-                <li className="flex gap-2"><Check /> Divulgação no Evento</li>
-              </ul>
-              <div className="w-full h-20 flex justify-center items-center border-t">
-                <p className="font-bold text-3xl ">
-                  R$ 80,00
-                </p>
-              </div>
-            </div>
-
-            <div className="bg-primary w-full h-[390px] flex flex-col gap-3 justify-center items-center border border-gray-200 rounded-lg">
-              <div className="w-full h-16 border-b border-gray-500 flex justify-center items-center">
-                <h3 className="font-bold text-xl"><span className="text-3xl font-medieval font-medium">E</span>scudeiro</h3>
-              </div>
-              <ul className="w-full h-full flex flex-col gap-2 px-2">
-                <li className="flex gap-2"><Check /> Logo no Banner do evento</li>
-                <li className="flex gap-2 text-gray-600"> Logo no site</li>
-                <li className="flex gap-2 text-gray-600"> Bandeiras na arena com a logo</li>
-                <li className="flex gap-2 text-gray-600"> Logo em material</li>
-                <li className="flex gap-2 text-gray-600"> Divulgação da marca em vídeo</li>
-                <li className="flex gap-2"><Check /> Divulgação no Evento</li>
-              </ul>
-              <div className="w-full h-20 flex justify-center items-center border-t">
-                <p className="font-bold text-3xl ">
-                  R$ 40,00
-                </p>
-              </div>
-            </div>
-
-          </div>
+          <Plans />
         </div>
       </AnimatedContainer>
 
--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -2,12 +2,13 @@ import { CardBody, CardContainer } from "@ui/components/3dCard";
 import { AnimatedContainer } from "@ui/components/AnimatedContainer";
 import { motion } from "framer-motion"
 import { Banner } from "@ui/components/Banner";
-import { BicepsFlexed, Calendar, Check, Mail, Scroll, ShieldBanIcon, Sword, Swords, User } from "lucide-react";
+import { BicepsFlexed, Calendar, Mail, Scroll, ShieldBanIcon, Sword, Swords, User } from "lucide-react";
 import { Link } from "react-router-dom";
 import { NextEventCard } from "@ui/components/NextEventCard";
 import { useEffect } from "react";
 import { HeroSection } from "@ui/components/HeroSection";
 import { Rules } from "@ui/components/Rules";
+import { Plans } from "@ui/components/Plans";
 
 export default function Home() {
   useEffect(() => {
@@ -285,66 +286,7 @@ export default function Home() {
                 </p>
               </div>
 
-              <div className="w-full flex flex-col md:flex-row justify-center items-center gap-4 md:gap-0 xl:gap-8 text-white mt-8">
-
-                <div className="bg-primary w-full h-[390px] flex flex-col gap-3 justify-center items-center border border-gray-200 rounded-lg">
-                  <div className="w-full h-16 border-b border-gray-500 flex justify-center items-center">
-                    <h3 className="font-bold text-xl"><span className="text-3xl font-medieval font-medium">N</span>obre</h3>
-                  </div>
-                  <ul className="w-full h-full flex flex-col gap-2 px-2">
-                    <li className="flex gap-2"><Check /> Logo no Banner do evento</li>
-                    <li className="flex gap-2"><Check /> Logo no site</li>
-                    <li className="flex gap-2"><Check /> Bandeiras na arena com a logo</li>
-                    <li className="flex gap-2"><Check /> Logo em material</li>
-                    <li className="flex gap-2"><Check /> Divulgação da marca em vídeo</li>
-                    <li className="flex gap-2"><Check /> Divulgação no Evento</li>
-                  </ul>
-                  <div className="w-full h-20 flex justify-center items-center border-t">
-                    <p className="font-bold text-3xl ">
-                      R$ 100,00
-                    </p>
-                  </div>
-                </div>
-
-                <div className="bg-primary w-full h-[390px] flex flex-col gap-3 justify-center items-center border border-gray-200 rounded-lg">
-                  <div className="w-full h-16 border-b border-gray-500 flex justify-center items-center">
-                    <h3 className="font-bold text-xl"><span className="text-3xl font-medieval font-medium">C</span>avaleiro</h3>
-                  </div>
-                  <ul className="w-full h-full flex flex-col gap-2 px-2">
-                    <li className="flex gap-2"><Check /> Logo no Banner do evento</li>
-                    <li className="flex gap-2"><Check /> Logo no site</li>
-                    <li className="flex gap-2 text-gray-600">Bandeiras na arena com a logo</li>
-                    <li className="flex gap-2 text-gray-600"> Logo em material</li>
-                    <li className="flex gap-2"><Check /> Divulgação da marca em vídeo</li>
-                    <li className="flex gap-2"><Check /> Divulgação no Evento</li>
-                  </ul>
-                  <div className="w-full h-20 flex justify-center items-center border-t">
-                    <p className="font-bold text-3xl ">
-                      R$ 80,00
-                    </p>
-                  </div>
-                </div>
-
-                <div className="bg-primary w-full h-[390px] flex flex-col gap-3 justify-center items-center border border-gray-200 rounded-lg">
-                  <div className="w-full h-16 border-b border-gray-500 flex justify-center items-center">
-                    <h3 className="font-bold text-xl"><span className="text-3xl font-medieval font-medium">E</span>scudeiro</h3>
-                  </div>
-                  <ul className="w-full h-full flex flex-col gap-2 px-2">
-                    <li className="flex gap-2"><Check /> Logo no Banner do evento</li>
-                    <li className="flex gap-2 text-gray-600"> Logo no site</li>
-                    <li className="flex gap-2 text-gray-600"> Bandeiras na arena com a logo</li>
-                    <li className="flex gap-2 text-gray-600"> Logo em material</li>
-                    <li className="flex gap-2 text-gray-600"> Divulgação da marca em vídeo</li>
-                    <li className="flex gap-2"><Check /> Divulgação no Evento</li>
-                  </ul>
-                  <div className="w-full h-20 flex justify-center items-center border-t">
-                    <p className="font-bold text-3xl ">
-                      R$ 40,00
-                    </p>
-                  </div>
-                </div>
-
-              </div>
+              <Plans />
 
               <div className="w-full flex flex-col justify-center items-center mt-5">
                 <p className="text-lg">Ficou interessado? Entre em contato conosco:</p>
